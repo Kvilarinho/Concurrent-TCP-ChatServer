c04ee47e685fda21899c28fcf44a25ab2608fbfb
Kvilarinho <katia.vilas@gmail.com>
2025-11-11 20:02:01 +0000

    Initialize ChatServer and Client projects

---
+++ ChatServer/src/main/java/com/codeforall/online/chatserver/ChatServer.java
@@ -0,0 +1,13 @@
+package com.codeforall.online.chatserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class ChatServer
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
